b99d03cd2ab820cf7388bd12e1d98dc565889097
Mohammad-Ghalib <92501236+Mohammad-Ghalib@users.noreply.github.com>
2023-01-31 02:48:58 +0530

    Day 8

--- Day8.cpp
+++ Day8.cpp
@@ -45,3 +45,72 @@
 
 //3 Job sequencing problem
 
+    static bool comparison(Job a, Job b){
+        return a.profit > b.profit;
+    }
+    
+    vector<int> JobScheduling(Job arr[], int n){
+        
+        sort(arr, arr+n, comparison);
+        int maxi = arr[0].dead, jobs=0, ans=0;
+        
+        for(int i=1; i<n; i++) maxi = max(maxi, arr[i].dead);
+        
+        int slot[maxi+1];
+        memset(slot, -1, sizeof(slot));
+        
+        for(int i=0; i<n; i++){
+            for(int j=arr[i].dead; j>0; j--){
+                if(slot[j] == -1){
+                    slot[j] = arr[i].id;
+                    ans += arr[i].profit;
+                    jobs++;
+                    break;
+                }
+            }
+        }
+        
+        return {jobs, ans};
+    } 
+
+//4 Fractional Knapsack
+
+    static bool compare(Item i1, Item i2){
+        return (double)i1.value/(double)i1.weight > (double)i2.value/(double)i2.weight;
+    }
+    
+    double fractionalKnapsack(int W, Item arr[], int n){
+        sort(arr, arr+n, compare);
+        
+        double ans=0;
+        
+        for(int i=0; i<n && W>0; i++){
+            if(W >= arr[i].weight){
+                ans += arr[i].value; 
+                W -= arr[i].weight;
+            }
+            else{
+                ans += ((double)arr[i].value/(double)arr[i].weight)*(double)W;
+                W=0;
+            }
+        }
+        
+        return ans;
+    }
+
+//5 Greedy Algo to find min. no. of coins 
+//(will work only if c[i] > c[i-1]+c[i-2])
+
+	int minCoins(int coins[], int M, int V){ 
+	    sort(coins, coins+M, greater<int>());
+	    int ans=0, i=0;
+	    while(V && i<M){
+	        ans += V/coins[i];
+	        V -= (V/coins[i])*coins[i];
+	        i++;
+	    }
+	    
+	    return ans;
+	} 
+
+//6 -> Same as N meetings in 1 room
