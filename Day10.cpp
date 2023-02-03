33afb019dbb5e4c4198fade3b5ba1cec29bc35b3
Mohammad-Ghalib <92501236+Mohammad-Ghalib@users.noreply.github.com>
2023-02-03 23:58:28 +0530

    Day 10

--- Day10.cpp
+++ Day10.cpp
@@ -86,3 +86,92 @@
 
 //4 M coloring problem
 
+    bool isSafe(int node, vector<int>& color, bool graph[101][101], int n, int col){
+        for(int j=0; j<n; j++){
+            if(j!=node && graph[node][j] && color[j] == col) return false;
+        }
+        return true;
+    }
+    
+    bool solve(int node, vector<int>& color, bool graph[101][101], int m, int n){
+        
+        if(node==n) return true;
+        
+        for(int i=1; i<=m; i++){
+            if(isSafe(node, color, graph, n, i)){
+                color[node] = i;
+                if(solve(node+1, color, graph, m, n)) return true;
+                color[node] = 0;
+            }
+        }
+        return false;
+    }
+    
+    bool graphColoring(bool graph[101][101], int m, int n) {
+        vector<int> color(n,0);
+        color[0] = 1;
+        return solve(0, color, graph, m, n);
+    }
+
+//5 Rat in a maze
+
+    int dr[4] = {1,0,0,-1};
+    int dc[4] = {0,-1,1,0};
+    
+    void solve(int r, int c, vector<vector<int>> &m, int n, vector<vector<int>>& vis, vector<string>& ans, string& s){
+        if(r==n-1 && c==n-1){
+            ans.push_back(s);
+            return;
+        }
+        
+        for(int i=0; i<4; i++){
+            int nr = r+dr[i], nc = c+dc[i];
+            if(nr>=0 && nc>=0 && nr<n && nc<n && m[nr][nc] && !vis[nr][nc]){
+                switch(i){
+                    case 0: s += 'D'; break;
+                    case 1: s += 'L'; break;
+                    case 2: s += 'R'; break;
+                    case 3: s += 'U'; break;
+                }
+                vis[r][c] = 1;
+                solve(nr, nc, m, n, vis, ans, s);
+                s.pop_back();
+                vis[r][c] = 0;
+            }
+        }
+    }
+    
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        if(!m[0][0] || !m[n-1][n-1]) return ans;
+        
+        vector<vector<int>> vis(n, vector<int>(n,0));
+        string s;
+        solve(0, 0, m, n, vis, ans, s);
+        return ans;
+    }
+
+//6 Word break
+
+    bool solve(int i, string s, map<string,int>& mp, vector<int>& dp){
+        if(i==s.size()) return true;
+        if(dp[i] != -1) return dp[i];
+        string temp;
+        for(int j=i; j<s.size(); j++){
+            temp += s[j];
+            if(mp[temp]){
+                if(solve(j+1, s, mp, dp)) return dp[i] = true;
+            }
+        }
+        return dp[i] = false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        map<string, int> mp;
+        vector<int> dp(s.size(), -1);
+        for(int i=0; i<wordDict.size(); i++){
+            mp[wordDict[i]]++;
+        }
+
+        return solve(0, s, mp, dp);
+    }
